b2f4f37945ec5121a1730813565ee7affb023536
Albert <albert.jakobi@gmail.com>
2024-07-18 00:39:04 +0200

    Add Driver model and migration for drivers table

---
+++ app/Models/Driver.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Driver extends Model
+{
+    use HasFactory;
+
+    protected $table = 'drivers';
+
+    protected $fillable = [
+        'driverId',
+        'code',
+        'url',
+        'givenName',
+        'familyName',
+        'dateOfBirth',
+        'nationality'
+    ];
+
+    protected $primaryKey = 'driverId';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    public $timestamps = true;
+
+    protected $casts = [
+        'dateOfBirth' => 'date',
+    ];
+}
---
+++ database/migrations/2024_07_17_221944_create_drivers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->string('driverId')->unique();
+            $table->string('code')->nullable();
+            $table->string('url');
+            $table->string('givenName');
+            $table->string('familyName');
+            $table->date('dateOfBirth');
+            $table->string('nationality');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drivers');
+    }
+};
